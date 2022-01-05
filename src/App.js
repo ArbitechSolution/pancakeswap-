d58fb250b5ce43a53cfc61abd6b95b15f00f75f8
ArbitechSolution <arbitechsolutions@gmail.com>
2022-01-05 17:19:36 +0500

    First Commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+// import Cards from './Componen2/AllCards/Cards';
+import Home from './Componen2/Home/Home';
+import './App.css';
+import Card from './Componen2/ChecktheCompoente/Card';
+import Checkchard from './Componen2/AllCards/checkchard';
+import Newcard from './Componen2/AllCards/Newcard';
+import {Checkhere} from './Componen2/ChecktheCompoente/Checkcompoente';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <Card/>
+  <Checkchard/> 
+  <Newcard/>
+  {/* <Checkhere/> */}
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componen2/HistoryCard/HistoryCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Historystyle.css'
+
+export default function HistoryCard() {
+    return (
+        <div>
+        
+        
+
+            
+        </div>
+    )
+}
